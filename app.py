d27d0d737fb1b3e1ef8ac573e574a800c7266457
Harshith Alfred <harshithalfred@gmail.com>
2026-04-14 12:57:30 +0530

    Refactor app to support dynamic port and host
    Updated the app to use environment variable for port and set host to '0.0.0.0'.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from inference_sdk import InferenceHTTPClient
 import tempfile
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -38,5 +38,8 @@ def upload():
         "lng": float(lng)
     })
 
+
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
